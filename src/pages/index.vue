3acdbcf60ba612c982f989ab462e04ed53985e1c
kawksung <alstjd5479@naver.com>
2024-02-07 23:16:30 +0900

    chore: router 및 alis 설정

---
+++ src/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>INDEX.VUE 페이지</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,30 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@apis": fileURLToPath(new URL("./src/apis", import.meta.url)),
+      "@assets": fileURLToPath(new URL("./src/assets", import.meta.url)),
+      "@components": fileURLToPath(
+        new URL("./src/components", import.meta.url)
+      ),
+      "@pages": fileURLToPath(new URL("./src/pages", import.meta.url)),
+      "@router": fileURLToPath(new URL("./src/router", import.meta.url)),
+      "@store": fileURLToPath(new URL("./src/store", import.meta.url)),
+    },
+  },
+  // scss 전역 사용
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalDate: `@import "./src/assets/styles/main.scss"`,
+      },
+    },
+  },
+});
